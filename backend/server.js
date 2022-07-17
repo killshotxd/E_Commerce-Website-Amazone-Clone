ae0c00ee50f07a0c19d5d2954c06ff35be0e49ac
killshotxd <hassanansari211@gmail.com>
2022-07-17 10:49:53 +0530

    change lint

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import data from "./data.js";
-const app = express();
+import data from "./data";
 
+const app = express();
 app.use(cors());
 app.get("/api/products", (req, res) => {
   res.send(data.products);
